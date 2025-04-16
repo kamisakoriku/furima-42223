8998ea0baafc613a28d1d6b9f7496e3079b5ce6e
kamisakoriku <lb8kstvf29ro.e@gmail.com>
2025-04-16 20:27:47 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -24,10 +24,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+
     unless user_signed_in?
       redirect_to new_user_session_path
       return
     end
+
     return if current_user.id == @item.user_id
 
     redirect_to root_path
