60edddc2737f99424098801e8bfcd10a41b52315
kamisakoriku <lb8kstvf29ro.e@gmail.com>
2025-04-16 18:07:32 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show, :edit, :destroy]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
